723b9605523b463dabeb8b7ff7093320aacc7b3b
Matt Tobin <matthew.tobin@oliverwyman.com>
2022-07-07 12:08:33 +0100

    Added prettier + formatted all files

--- src/components/Counter.cy.tsx
+++ src/components/Counter.cy.tsx
@@ -1,29 +1,29 @@
-import Counter from "./Counter";
+import Counter from './Counter';
 
-describe("Counter", () => {
-  it("mounts", () => {
+describe('Counter', () => {
+  it('mounts', () => {
     cy.mount(<Counter />);
   });
 
-  it("should default to 0", () => {
+  it('should default to 0', () => {
     cy.mount(<Counter />);
-    cy.findByTestId("counter").should("contain.text", "0");
+    cy.findByTestId('counter').should('contain.text', '0');
   });
 
-  it("should set a default value via a prop", () => {
+  it('should set a default value via a prop', () => {
     cy.mount(<Counter initialValue={200} />);
-    cy.findByTestId("counter").should("contain.text", "200");
+    cy.findByTestId('counter').should('contain.text', '200');
   });
 
-  it("should increment the count", () => {
+  it('should increment the count', () => {
     cy.mount(<Counter />);
-    cy.findByRole("button", { name: "+" }).click();
-    cy.findByTestId("counter").should("contain.text", "1");
+    cy.findByRole('button', { name: '+' }).click();
+    cy.findByTestId('counter').should('contain.text', '1');
   });
 
-  it("should decrement the count", () => {
+  it('should decrement the count', () => {
     cy.mount(<Counter />);
-    cy.findByRole("button", { name: "-" }).click();
-    cy.findByTestId("counter").should("contain.text", "-1");
+    cy.findByRole('button', { name: '-' }).click();
+    cy.findByTestId('counter').should('contain.text', '-1');
   });
 });
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Counter({ initialValue = 0 }) {
   const [count, setCount] = useState(initialValue);
 
   return (
     <div data-testid="counter" className="p-4 flex items-center">
-      <button
-        className="bg-blue-500 text-white px-3 py-1 rounded"
-        onClick={() => setCount(count - 1)}
-      >
+      <button className="bg-blue-500 text-white px-3 py-1 rounded" onClick={() => setCount(count - 1)}>
         -
       </button>
       <p className="text-xl font-semibold mx-4">{count}</p>
-      <button
-        className="bg-blue-500 text-white px-3 py-1 rounded"
-        onClick={() => setCount(count + 1)}
-      >
+      <button className="bg-blue-500 text-white px-3 py-1 rounded" onClick={() => setCount(count + 1)}>
         +
       </button>
     </div>
