2ccf55daff0e44fc10ce4cfb19c082dd551e856a
Matt Tobin <matthew.tobin@oliverwyman.com>
2022-07-07 12:06:53 +0100

    Added cypress and Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Counter from "./components/Counter";
+
 function App() {
   return (
     <div className="container mx-auto px-4">
       <header className="py-4">
         <h1 className="text-2xl">Cypress Component Testing</h1>
       </header>
+
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.cy.tsx
@@ -0,0 +1,29 @@
+import Counter from "./Counter";
+
+describe("Counter", () => {
+  it("mounts", () => {
+    cy.mount(<Counter />);
+  });
+
+  it("should default to 0", () => {
+    cy.mount(<Counter />);
+    cy.findByTestId("counter").should("contain.text", "0");
+  });
+
+  it("should set a default value via a prop", () => {
+    cy.mount(<Counter initialValue={200} />);
+    cy.findByTestId("counter").should("contain.text", "200");
+  });
+
+  it("should increment the count", () => {
+    cy.mount(<Counter />);
+    cy.findByRole("button", { name: "+" }).click();
+    cy.findByTestId("counter").should("contain.text", "1");
+  });
+
+  it("should decrement the count", () => {
+    cy.mount(<Counter />);
+    cy.findByRole("button", { name: "-" }).click();
+    cy.findByTestId("counter").should("contain.text", "-1");
+  });
+});
